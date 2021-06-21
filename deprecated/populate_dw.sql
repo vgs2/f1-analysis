d4146ab3c53b246a9ea73e12cca188d5f4d0ff2a
Silvana Lílian <75362803+silvanalilian@users.noreply.github.com>
2021-06-21 09:04:42 -0300

    Add files via upload

---
+++ deprecated/populate_dw.sql
@@ -0,0 +1,14 @@
+NSERT INTO dim_piloto
+SELECT driverId, (forename || ' ' || surname), date_part('year',dob), nationality
+FROM drivers;
+INSERT INTO dim_construtor
+SELECT constructorId,name, nationality
+FROM constructors;
+INSERT INTO dim_tempo
+SELECT distinct(date), EXTRACT(day FROM  date(date)) as day, EXTRACT(month FROM  date(date)) as month, EXTRACT(year FROM  date(date)) as year
+FROM races
+INSERT INTO dim_circuito
+SELECT circuitId,name, country,lat,lng,alt
+FROM circuits;
+SELECT distinct(date), EXTRACT(year FROM  date(date)) as year, EXTRACT(month FROM  date(date)) as month,
+EXTRACT(day FROM  date(date)) as day FROM races
